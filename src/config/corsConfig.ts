7280998b872c4b31976021e38408487dc6467428
Анна <anna.kuvakina91@gmail.com>
2025-07-13 23:52:24 +0300

    Update corsConfig.ts

--- src/config/corsConfig.ts
+++ src/config/corsConfig.ts
@@ -1,6 +1,9 @@
 const corsConfig = {
-  origin: ['https://flavorai-front.vercel.app', 'http://localhost:5173'],
-    credentials: true,
-  };
-  
+  origin: [
+    'https://flavorai-front-1xysazctz-hannas-projects-07daf4ab.vercel.app',
+    'http://localhost:5173',
+  ],
+  credentials: true,
+};
+
 export default corsConfig;
